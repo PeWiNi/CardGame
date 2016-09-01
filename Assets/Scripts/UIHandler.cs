2594a279e0f2475fe5f6a3dd8fb8d24b60b70f11
cristinaNiculescu <crissa_alessa@yahoo.com>
2016-09-01 14:22:06 +0200

    :sailboat:

---
+++ Assets/Scripts/UIHandler.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIHandler : MonoBehaviour {
+	public GameObject mainMenu; 
+	public GameObject matchMenu;
+	public GameObject settingsMenu;
+	public GameObject cardCollection;
+	public GameObject shopMenu;
+	public GameObject achievementList;
+
+
+
+	// Use this for initialization
+	void Start () {
+		//mainMenu = GameObject.Find("Main_Menu");
+		mainMenu.SetActive (true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void findGame()
+	{
+		Debug.Log ("start game");
+	}
+
+}
